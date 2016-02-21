71bc55964921972fccb6687494fd0a0d74148749
Valeriy Ryzhov <valeriy.rvv@gmail.com>
2016-02-22 01:56:27 +0600

    Добавил обработчики модального окна обратной связи и окны карты в файле script.js.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/*-Обработчик переключателей пунктов меню "Сервисы"-*/
+
 var serviceLinks = document.querySelectorAll(".service-menu-column a");
 var serviceBlocks = document.querySelectorAll(".service-details-page");
 
@@ -20,3 +22,46 @@ for (i = 0; i < serviceLinks.length; ++i) {
         }
     })
 }
+
+/*-Обработчик молального окна обратной связи-*/
+
+var ModalWindow = document.querySelector(".modal-window");
+var CompanyFeedbackButton = document.querySelector(".company-feedback-button");
+var ModalWindowClose = document.querySelector(".modal-button-close");
+
+
+CompanyFeedbackButton.addEventListener("click", function (event) {
+    event.preventDefault(event);
+    ModalWindow.classList.add("window-appearance");
+});
+
+ModalWindowClose.addEventListener("click", function (event) {
+    event.preventDefault(event);
+    ModalWindow.classList.remove("window-appearance");
+});    
+
+window.addEventListener("keydown", function (event) {
+    if (event.keyCode == 27) {
+        if (ModalWindow.classList.contains("window-appearance")) {
+            ModalWindow.classList.remove("window-appearance");
+        }
+    }
+});
+
+/*-Обработчик окна карты-*/
+
+var HoverMapWindow = document.querySelector(".hover-map");
+var HoverMapButton = document.querySelector(".company-contacts-map");
+
+HoverMapButton.addEventListener("click", function (event) {
+    event.preventDefault(event);
+    HoverMapWindow.classList.add("window-appearance");
+});
+
+window.addEventListener("keydown", function (event) {
+    if (event.keyCode == 27) {
+        if (HoverMapWindow.classList.contains("window-appearance")) {
+            HoverMapWindow.classList.remove("window-appearance");
+        }
+    }
+});
